a1497e1c75f5e1fafeec73ba821fbf8cec80099b
lea <lea@MOUTON>
2018-02-05 10:12:42 +0100

    addition guigui

--- Projet_Zombi/Program.cs
+++ Projet_Zombi/Program.cs
@@ -10,6 +10,7 @@ namespace Projet_Zombi
     {
         static void Main(string[] args)
         {
+            int guigui = 0;
             Console.WriteLine("Hello World");
             Console.WriteLine("Hello World");
         }
