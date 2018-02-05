52e6a63df00534c97ea15fae3aee2d10495e291d
lea <lea@MOUTON>
2018-02-05 10:06:39 +0100

    deuxième hello

--- Projet_Zombi/Program.cs
+++ Projet_Zombi/Program.cs
@@ -11,6 +11,7 @@ namespace Projet_Zombi
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
