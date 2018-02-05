2c9b4999c29ee7a52e8cdf4c46771925e5eab49c
Guillaume COUZINET <guillaume.couzinet@devinci.fr>
2018-02-05 11:58:51 +0100

    Attributs de personnes

--- Projet_Zombi/Personne.cs
+++ Projet_Zombi/Personne.cs
@@ -8,5 +8,15 @@ namespace Projet_Zombi
 {
     class Personne
     {
+        private int matricule;
+        private string nom;
+        private string prenom;
+        private int sexe;
+        private int age;
+        private string fonction;
+        private int cagnotte;
+        private string grade;
+        private string pouvoirs;
+
     }
 }
